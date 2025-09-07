7677da23f5635bc8a51de1635b8c57d60188f5a4
allen9630308 <allen9630308@gmail.com>
2025-09-07 12:47:38 +0800

    Update URL

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/shopping-demo/',
+  base: '/Shopping-Cart/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
